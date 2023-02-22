e474c0d03b1bf10ff1009e1732bb8923afce0766
Xanda Schofield <xanda@cs.hmc.edu>
2023-02-22 14:27:51 -0800

    Added movie function comments

--- process_movie_data.py
+++ process_movie_data.py
@@ -4,18 +4,29 @@ import csv
 
 
 def find_top_5(filename):
-    """Finds the top 5 highest grossing movies in a CSV dataset."""
+    """Finds the top 5 highest grossing movies in a CSV dataset.
+       Input: filename, a string - points to filename of dataset
+       Output: None
+       Effect: should print five lines of text
+    """
+    # read in file contents as list of dictionaries
     with open(filename) as f:
         csvr = csv.DictReader(f)
         rows = [r for r in csvr]
     
+    # Sort data and get top 5
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
     top_five = rows[:-5:-1]
 
+    # Print out results
     for row in top_five:
         print("{row[Title]} ({row[Release Date]}) - ${row[Gross]}".format(
             row=row))
 
+
+# Script to run
+# Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
+# https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
     find_top_5("Movies_gross_rating.csv")
