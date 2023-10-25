738973a4c05dca9fa633dccb56a492d7038831fa
Nico Villalba <nicholasvillalbae@gmail.com>
2023-10-24 20:51:59 -0700

    made top_N function

--- process_movie_data.py
+++ process_movie_data.py
@@ -3,8 +3,8 @@
 import csv
 
 
-def find_top_5(filename):
-    """Finds the top 5 highest grossing movies in a CSV dataset.
+def find_top_n(filename, n=5):
+    """Finds the top N highest grossing movies in a CSV dataset.
        Input: filename, a string - points to filename of dataset
        Output: None
        Effect: should print five lines of text
@@ -19,13 +19,13 @@ def find_top_5(filename):
         row["Gross"] = int(row["Gross"])
         row["Year"] = int(row["Release Date"][:4])
 
-    # Sort data and get top 5
+    # Sort data and get top N
     gross_sort = lambda x : x["Gross"]
     rows.sort(key=gross_sort)
-    top_five = rows[:-6:-1]
+    top_n = rows[:-n-1:-1]
 
     # Print out results
-    for i, row in enumerate(top_five):
+    for i, row in enumerate(top_n):
         print("{ind}. {row[Title]} ({row[Year]}) - ${row[Gross]:,d}".format(
             ind=i+1,
             row=row))
@@ -35,4 +35,4 @@ def find_top_5(filename):
 # Movie data comes from "Movie Gross and Ratings" dataset on Kaggle by Yashwanth Sharaf
 # https://www.kaggle.com/datasets/thedevastator/movie-gross-and-ratings-from-1989-to-2014
 if __name__ == "__main__":
-    find_top_5("Movies_gross_rating.csv")
+    find_top_n("Movies_gross_rating.csv")
